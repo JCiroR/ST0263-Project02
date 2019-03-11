48ea924aa4b696693f7c1a3c504d1c7ee9369e94
Essux <jjsuarestra99@gmail.com>
2019-03-11 00:26:55 -0500

    Restructuring folders

---
+++ cpp/stopword_cleaner.py
@@ -0,0 +1,17 @@
+print('Loading libraries')
+from sklearn.feature_extraction import text
+import pandas as pd
+
+dfs = []
+stop = text.ENGLISH_STOP_WORDS
+
+def remove_stopwords(st):
+    return ' '.join([x.lower().strip('.?!()" ,;-_”[]()’“&^`’*') for x in st.split() if x.lower() not in stop])
+
+for i in range(3):
+    print('Reading file', i+1)
+    df = pd.read_csv('data/articles{}.csv'.format(i+1))
+    print('Processing file', i+1)
+    df = df[['id', 'title', 'content']]
+    df['content'] = df['content'].map(remove_stopwords)
+    df.to_csv('data/articles{}_semicolon.csv'.format(i+1), header=False, encoding='utf-8', index=False, sep=';')
--- python/serial.py
+++ python/serial.py
@@ -1,12 +1,16 @@
+import cProfile, pstats, io
+
 from collections import Counter
 import heapq
 import pandas as pd
 import json
 print('Loaded libraries')
 
+pr = cProfile.Profile()
+pr.enable()
 K = 10
 
-df = pd.read_csv('data/all_data.csv')
+df = pd.read_csv('data/sample.csv')
 df = df.dropna()
 print('Loaded data')
 
@@ -15,16 +19,16 @@ inverse_index = {}
 processed_rows = 0
 for row in df.itertuples():
     row_id = int(row.id)
-    try:
-        assert(isinstance(row.content, str))
-    except AssertionError:
-        print(row.content)
-        continue
     c = Counter(row.content.split())
     for key, value in c.items():
         if key not in inverse_index:
             inverse_index[key] = []
-        inverse_index[key].append((row_id, row.title, value))
+        #inverse_index[key].append((row_id, row.title, value))
+        if len(inverse_index[key]) == K:
+            heapq.heappushpop(inverse_index[key], (value, row_id, row.title))
+        else:
+            heapq.heappush(inverse_index[key], (value, row_id, row.title))
+
     processed_rows += 1
     if processed_rows%1000==0:
         print(processed_rows, 'rows processed')
@@ -32,10 +36,16 @@ for row in df.itertuples():
 
 processed_words = 0
 for key, value in inverse_index.items():
-    inverse_index[key] = heapq.nlargest(K, value, key=lambda x : x[2])
+    inverse_index[key].sort(reverse=True)
     processed_words += 1
     if processed_words%10000==0:
         print(processed_words, 'words processed')
 
+pr.disable()
+s = open('profile.txt', 'w')
+ps = pstats.Stats(pr, stream=s).sort_stats('tottime')
+ps.print_stats()
+
 with open('output.txt', 'w', encoding='utf-8') as f:
     json.dump(inverse_index, f, ensure_ascii=False)
+
